c98db3cf83f8443d0e661506f75dcd11064c2ed5
MADESH P <madesh20.cse@gmail.com>
2026-08-23 14:41:11 +0530

    Create 02_Majority-Element.java

---
+++ Top_50/02_Majority-Element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+         HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0) + 1);
+        }
+        int x = nums.length / 2;
+        for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i]) > x){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
